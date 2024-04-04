220192d0b8681c3a8b3072cb18ecaff9c18dab0a
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-04-04 12:49:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+ function superbowlWin (records) {
+    let win = records.find(record => record.result === "W");
+    return win ? win.year : undefined
+ }
+
+ superbowlWin()
+
+console.log(superbowlWin());
+
+
